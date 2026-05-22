2aebfcc3d1c05237fbef16df96c1934195f2fe3d
Gustavo Pedrosa <luizgfju@hotmail.com>
2026-05-21 22:15:46 -0300

    aula de tuplas

--- 02-Fundamentos/11-metodos_lista.py
+++ 02-Fundamentos/11-metodos_lista.py
@@ -9,7 +9,19 @@ print(len(listaFilmes))
 print(listaFilmes.index("xXx"))
 
 # Adicionar um filme ao final da lista
-
-print(
-    f"Adicionando filme: O senhor dos anéis na lista: {listaFilmes.append("O senhor dos anéis")}\n posição: {len(listaFilmes)} \nlista {listaFilmes}")
+listaFilmes.append("O senhor dos anéis")
 print(len(listaFilmes))
+
+# Ordenar a lista
+listaFilmes.sort()
+print(listaFilmes)
+
+# Copiar os itens de uma lista para outra
+copiaFilmes = listaFilmes.copy()  # Copiei para uma nova lista
+print(copiaFilmes)
+copiaFilmes.remove("xXx")  # remove um item da lista
+print(copiaFilmes)
+
+# Remove todos os itens de uma lista
+listaFilmes.clear()  # Apaga toda a lista
+print(listaFilmes)
